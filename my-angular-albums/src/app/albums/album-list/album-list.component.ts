8dee0b53da22d41e63901d603a8eb346df937850
Shilpa Patel <shilpabpatel@gmail.com>
2019-09-26 13:31:17 -0400

    ch08-labs

--- my-angular-albums/src/app/albums/album-list/album-list.component.ts
+++ my-angular-albums/src/app/albums/album-list/album-list.component.ts
@@ -1,23 +1,55 @@
-import { Component, OnInit, Output, EventEmitter } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+
+
+
+import { AlbumService } from "../shared/album.service"
+
 import { Album } from "../album.model";
-import { ALBUMS } from "../albums.data";
+
+import { Observable } from 'rxjs';
+
+
 
 @Component({
+
   selector: "app-album-list",
+
   templateUrl: "./album-list.component.html",
+
   styleUrls: ["./album-list.component.css"]
+
 })
+
 export class AlbumListComponent implements OnInit {
-  constructor() {}
-  albumsArray: Album[];
+
+  albumsArray: Observable<Album[]>;
+
+
+
+  constructor(private albumService: AlbumService) { }
+
+
 
   ngOnInit(): void {
-    this.albumsArray = this.albumsArray = ALBUMS;
+
+    this.getAlbums();
+
    }
-   
+
+
+
   parentFunctionHandler(album) {
-    alert(
-      "Album " + album.albumName + " was sent from the album card component"
-    );
+
+    alert('Album ' + album.albumName + ' was sent from the album card component');
+
   }
+
+  
+
+  getAlbums() {
+
+    this.albumsArray = this.albumService.getAlbums();
+
+  }
+
 }
